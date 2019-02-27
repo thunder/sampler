ce21bf92d648f8e6c67043949a4dff5230e894a9
Daniel Bosen <daniel.bosen@burda.com>
2019-02-27 15:28:00 +0100

    User and node data tests.

--- tests/src/Functional/SamplerFunctionalTest.php
+++ tests/src/Functional/SamplerFunctionalTest.php
@@ -5,42 +5,77 @@ namespace Drupal\Tests\sampler\Functional;
 class SamplerFunctionalTest extends SamplerFunctionalTestBase {
 
   /**
-   * Test sampling of data.
+   * Test sampling of user data.
    */
   public function testUserDataSampling() {
-    $nodeEditingRid1 = 'editor1';
-    $nodeEditingRid2 = 'editor2';
     $nonEditingRid = 'restricted';
+    $nodeEditingRid1 = 'node_editor_1';
+    $nodeEditingRid2 = 'node_editor_2';
+    $taxonomyEditingRid1 = 'term_editor_1';
 
-    $numberOfNodeEditors1 = 4;
-    $numberOfNodeEditors2 = 5;
-    $numberOfRestricted = 6;
+    $numberOfRestricted = 1;
+    $numberOfNodeEditors1 = 2;
+    $numberOfNodeEditors2 = 3;
+    $numberOfTaxonomyEditors1 = 4;
 
+    $this->createRole([], $nonEditingRid);
     $this->createRole(['create type_one content'], $nodeEditingRid1);
     $this->createRole(['create type_two content'], $nodeEditingRid2);
-    $this->createRole([], $nonEditingRid);
+    $this->createRole(['edit terms in vocabulary_one'], $taxonomyEditingRid1);
 
-    $this->createUsersForRole($nodeEditingRid1, $numberOfNodeEditors1);
-    $this->createUsersForRole($nodeEditingRid2, $numberOfNodeEditors2);
-    $this->createUsersForRole($nonEditingRid, $numberOfRestricted);
+    $this->createUsersWithRole($nonEditingRid, $numberOfRestricted);
+    $this->createUsersWithRole($nodeEditingRid1, $numberOfNodeEditors1);
+    $this->createUsersWithRole($nodeEditingRid2, $numberOfNodeEditors2);
+    $this->createUsersWithRole($taxonomyEditingRid1, $numberOfTaxonomyEditors1);
 
     $report = $this->container->get('sampler.reporter')
       ->setAnonymize(FALSE)
       ->collect()
       ->getReport();
-    file_put_contents('/tmp/debug-test.txt', json_encode($report, JSON_PRETTY_PRINT));
 
     $userReport = $report['user'];
 
     $this->assertEquals(17, $userReport['base_fields']);
+    $this->assertEquals($numberOfRestricted, $userReport['roles'][$nonEditingRid]['instances']);
     $this->assertEquals($numberOfNodeEditors1, $userReport['roles'][$nodeEditingRid1]['instances']);
     $this->assertEquals($numberOfNodeEditors2, $userReport['roles'][$nodeEditingRid2]['instances']);
-    $this->assertEquals($numberOfRestricted, $userReport['roles'][$nonEditingRid]['instances']);
+    $this->assertEquals($numberOfTaxonomyEditors1, $userReport['roles'][$taxonomyEditingRid1]['instances']);
 
     $this->assertEquals(($numberOfNodeEditors1 + $numberOfNodeEditors2), $userReport['editing_users']['node']['instances']);
+    $this->assertEquals($numberOfTaxonomyEditors1, $userReport['editing_users']['taxonomy']['instances']);
+  }
 
-    // Force fail while writing test, remove when finished with PR.
+  /**
+   * Test sampling of node data.
+   */
+  public function testNodeDataSampling() {
+    $nodeTypeOne = 'type_one';
+    $nodeTypeTwo = 'type_two';
+
+    $numberOfNodesTypeOne = 5;
+    $numberOfNodesTypeTwo = 6;
+
+    $this->createNodesOfType($nodeTypeOne, $numberOfNodesTypeOne);
+    $this->createNodesOfType($nodeTypeTwo, $numberOfNodesTypeTwo);
+
+    $report = $this->container->get('sampler.reporter')
+      ->setAnonymize(FALSE)
+      ->collect()
+      ->getReport();
+
+    file_put_contents('/tmp/debug.txt', json_encode($report, JSON_PRETTY_PRINT));
+
+    $nodeReport = $report['node'];
+
+    $this->assertEquals(18, $nodeReport['base_fields']);
+    $this->assertEquals($numberOfNodesTypeOne, $nodeReport['bundles'][$nodeTypeOne]['instances']);
+    $this->assertEquals($numberOfNodesTypeTwo, $nodeReport['bundles'][$nodeTypeTwo]['instances']);
+
+    $this->assertEquals(2, $nodeReport['bundles'][$nodeTypeOne]['fields']);
+    $this->assertEquals(0, $nodeReport['bundles'][$nodeTypeTwo]['fields']);
+
     $this->assertTrue(FALSE);
+
   }
 
 }
--- tests/src/Functional/SamplerFunctionalTestBase.php
+++ tests/src/Functional/SamplerFunctionalTestBase.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\sampler\Functional;
 
+use Drupal\node\Entity\Node;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\user\Entity\User;
 
@@ -19,29 +20,49 @@ abstract class SamplerFunctionalTestBase extends BrowserTestBase {
     'node',
     'field',
     'media',
+    'taxonomy',
     'sampler',
     'sampler_test',
   ];
 
   /**
-   * Create a given amount of users for a certain role.
+   * Create a given amount of users with a certain role.
    *
-   * @param int $rid
+   * @param string $rid
    *   The role id the users should have.
    * @param int $number
    *   The number of users to create.
    */
-  protected function createUsersForRole($rid, $number = 1) {
+  protected function createUsersWithRole(string $rid, int $number) {
     for ($i = 0; $i < $number; $i++) {
-      $edit = [];
-      $edit['name'] = !empty($name) ? $name : $this->randomMachineName();
-      $edit['mail'] = $edit['name'] . '@example.com';
-      $edit['pass'] = user_password();
-      $edit['status'] = 1;
-      $edit['roles'] = [$rid];
+      $user = [];
+      $user['name'] = !empty($name) ? $name : $this->randomMachineName();
+      $user['mail'] = $user['name'] . '@example.com';
+      $user['pass'] = user_password();
+      $user['status'] = 1;
+      $user['roles'] = [$rid];
 
-      $account = User::create($edit);
+      $account = User::create($user);
       $account->save();
+    }
+  }
+
+  /**
+   * Create a given amount of nodes of a certain type.
+   *
+   * @param string $type
+   *   The type of nodes to create.
+   * @param int $number
+   *   The number of nodes to create.
+   */
+  protected function createNodesOfType(string $type, int $number) {
+    for ($i = 0; $i < $number; $i++) {
+      $node = Node::create([
+        'type' => $type,
+        'title' => $this->randomString(),
+      ]);
+
+      $node->save();
     }
   }
 
