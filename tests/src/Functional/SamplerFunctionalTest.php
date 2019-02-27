2352ab6ba48f876d2880a307dd00f08dd18fa1d9
Daniel Bosen <daniel.bosen@burda.com>
2019-02-27 14:55:13 +0100

    Rename node types

--- tests/src/Functional/SamplerFunctionalTest.php
+++ tests/src/Functional/SamplerFunctionalTest.php
@@ -7,17 +7,37 @@ class SamplerFunctionalTest extends SamplerFunctionalTestBase {
   /**
    * Test sampling of data.
    */
-  public function testDataSampling() {
-    $editor = $this->drupalCreateUser(['create one content']);
-    $report = $this->container->get('sampler.reporter')->collect()->getReport();
+  public function testUserDataSampling() {
+    $nodeEditingRid1 = 'editor1';
+    $nodeEditingRid2 = 'editor2';
+    $nonEditingRid = 'restricted';
 
+    $numberOfNodeEditors1 = 4;
+    $numberOfNodeEditors2 = 5;
+    $numberOfRestricted = 6;
+
+    $this->createRole(['create type_one content'], $nodeEditingRid1);
+    $this->createRole(['create type_two content'], $nodeEditingRid2);
+    $this->createRole([], $nonEditingRid);
+
+    $this->createUsersForRole($nodeEditingRid1, $numberOfNodeEditors1);
+    $this->createUsersForRole($nodeEditingRid2, $numberOfNodeEditors2);
+    $this->createUsersForRole($nonEditingRid, $numberOfRestricted);
+
+    $report = $this->container->get('sampler.reporter')
+      ->setAnonymize(FALSE)
+      ->collect()
+      ->getReport();
     file_put_contents('/tmp/debug-test.txt', json_encode($report, JSON_PRETTY_PRINT));
 
-    $node_report = $report['node'];
-    $bundle = 'group-0';
+    $userReport = $report['user'];
 
-    $this->assertEquals(18, $node_report['base_fields']);
-    $this->assertEquals(2, $node_report['bundles'][$bundle]['fields']);
+    $this->assertEquals(17, $userReport['base_fields']);
+    $this->assertEquals($numberOfNodeEditors1, $userReport['roles'][$nodeEditingRid1]['instances']);
+    $this->assertEquals($numberOfNodeEditors2, $userReport['roles'][$nodeEditingRid2]['instances']);
+    $this->assertEquals($numberOfRestricted, $userReport['roles'][$nonEditingRid]['instances']);
+
+    $this->assertEquals(($numberOfNodeEditors1 + $numberOfNodeEditors2), $userReport['editing_users']['node']['instances']);
 
     // Force fail while writing test, remove when finished with PR.
     $this->assertTrue(FALSE);
--- tests/src/Functional/SamplerFunctionalTestBase.php
+++ tests/src/Functional/SamplerFunctionalTestBase.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\sampler\Functional;
 
 use Drupal\Tests\BrowserTestBase;
+use Drupal\user\Entity\User;
 
 /**
  * Base class for Sampler functional tests.
@@ -21,5 +22,27 @@ abstract class SamplerFunctionalTestBase extends BrowserTestBase {
     'sampler',
     'sampler_test',
   ];
+
+  /**
+   * Create a given amount of users for a certain role.
+   *
+   * @param int $rid
+   *   The role id the users should have.
+   * @param int $number
+   *   The number of users to create.
+   */
+  protected function createUsersForRole($rid, $number = 1) {
+    for ($i = 0; $i < $number; $i++) {
+      $edit = [];
+      $edit['name'] = !empty($name) ? $name : $this->randomMachineName();
+      $edit['mail'] = $edit['name'] . '@example.com';
+      $edit['pass'] = user_password();
+      $edit['status'] = 1;
+      $edit['roles'] = [$rid];
+
+      $account = User::create($edit);
+      $account->save();
+    }
+  }
 
 }
